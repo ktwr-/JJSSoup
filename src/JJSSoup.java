c4e3b16feb4d28fd2c6ca5d89ff95003eec2af60
takeuchi <kataware8136@gmail.com>
2015-10-29 18:24:51 +0900

    quotation change
    I can remove double quotation inside quotation,and quotation inside doubel quotation.
    also remove word inside quotation

--- src/JJSSoup.java
+++ src/JJSSoup.java
@@ -9,7 +9,7 @@ public class JJSSoup{
 	
 	public static void main(String args[]){
 		JJSSoup js = new JJSSoup();
-		StringBuilder sb = js.fileReader("./test/sample.js");
+		StringBuilder sb = js.fileReader("./test/test.js");
 		sb = js.InsertLinebreak(sb);
 		
 	}
@@ -39,10 +39,20 @@ public class JJSSoup{
 		System.out.println(sb.length());
 		int tmp=0;
 		// Srearch double quotation and quotation
-		dq = SearchWord(sb, "\"");
-		q = SearchWord(sb,"'");
+		dq = new ArrayList<Integer>(SearchWord(sb, "\""));
+		q = new ArrayList<Integer>(SearchWord(sb,"'"));
 		// remove quotation inside quotation and double quotation inseide quotation
+		modifyquote();
 		
+		for(int i=0;i< dq.size();i+=2){
+			System.out.println(sb.substring(dq.get(i), dq.get(i+1)+1));
+		}
+		for(int i=0;i< q.size();i+=2){
+			System.out.println(sb.substring(q.get(i), q.get(i+1)+1));
+		}
+		
+		ArrayList<Integer> semic = new ArrayList<Integer>(SearchWord(sb,";"));
+		semic = new ArrayList<Integer>(modifyWord(semic));
 		return sb;
 		
 	}
@@ -57,8 +67,64 @@ public class JJSSoup{
 		}
 		return tmp;
 	}
-	
-	private void modifyAL() {
+	/**
+	 * This function is remove word inside quotation and double quotation
+	 * @param word
+	 */
+	private ArrayList<Integer> modifyWord(ArrayList<Integer> word){
+		int i=0,j=0;
+		for(int k=0;k<word.size();k++){
+			System.out.println(word.get(k));
+		}
 		
+		
+		while(i < word.size() && j < dq.size()){
+				if(dq.get(j) < word.get(i) && word.get(i) <dq.get(j+1)){
+					word.remove(i);
+				}else if(dq.get(j+1)< word.get(i)){
+					j+=2;
+				}else if(word.get(i) < dq.get(j)){
+					i++;
+				}
+		}
+		i=0;
+		j=0;
+		while(i < word.size() && j < q.size()){
+				if(q.get(j) < word.get(i) && word.get(i) <q.get(j+1)){
+					word.remove(i);
+				}else if(q.get(j+1)< word.get(i)){
+					j+=2;
+				}else if(word.get(i) < q.get(j)){
+					i++;
+				}
+		}
+		for(int k=0;k<word.size();k++){
+			System.out.println(word.get(k));
+		}
+		return word;
 	}
+	
+	/**
+	 * This function is remove quotation inside double quotation,
+	 * and double quotation inside quotation
+	 */
+	private void modifyquote() {
+		int i=0,j=0;
+		while(i < dq.size() && j < q.size()){
+			if(dq.get(i) < q.get(j)){
+				if(q.get(j)< dq.get(i+1)){
+					q.remove(j);
+				}else if(dq.get(i+1) < q.get(j)){
+					i+=2;
+				}
+			}else if(q.get(j) < dq.get(i)){
+				if(dq.get(i)< q.get(j+1)){
+					dq.remove(i);
+				}else if(q.get(j+1) < dq.get(i)){
+					j+=2;
+				}
+			}
+		}	
+	}	
+		
 }
